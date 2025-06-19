de876d07ea0d7e685d186896997d8d61b17660e2
Clarice <clah.rice@hotmail.com>
2025-06-19 13:55:45 -0300

    Update SysbankV1.py
    Correção de erros de digitação e um operador faltante na verificação do saque.

--- SysbankV1.py
+++ SysbankV1.py
@@ -1,6 +1,6 @@
 # Passo 1: Criar variáveis para representar o saldo e a opção do menu
 
-saldo = 0.0
+saldo = 0.0 #Saldo inicial (o valor pode ser alterado)
 opcao = 0
 
 # Passo 2: Exibir uma mensagem de boas-vindas e as opções do menu
@@ -28,30 +28,25 @@ opcao = int(opcao_str)
 # Aqui você pode adicionar a lógica para cada opção do menu
 
 if opcao == 1:
-
     print(f"Seu saldo atual é: R$ {saldo:.2f}")
 
 elif opcao == 2:
-
     valor_deposito = float(input("Digite o valor a depositar: "))
     saldo += valor_deposito
     print(f"Depósito de R$ {valor_deposito:.2f} realizado com sucesso.")
     print(f"Seu novo saldo é: R$ {saldo:.2f}")
 
 elif opcao == 3:
-
     valor_saque = float(input("Digite o valor a sacar: "))
-    if valor_saque < saldo:
+    if valor_saque <= saldo:
         saldo -= valor_saque
         print(f"Saque de R$ {valor_saque:.2f} realizado com sucesso.")
         print(f"Seu novo saldo é: R$ {saldo:.2f}")
 
     else:
-
         print("Saldo insuficiente.")
 
 elif opcao == 4:
-
     print("Obrigado por utilizar nosso banco virtual!")
 
     '''Erros cometidos:
@@ -61,7 +56,7 @@ elif opcao == 4:
     2 - Eu confundi a linha opção = int(opcao_str) pra ficar como opcao_str = int(opcao_str) e ao colocar qualquer número
     o código não rodava os ifs e finalizava o programa.
     
-    Uma possível melhoria seria deixar as opções rodando até que o cliete escolha sair do programa, para que ele possa
+    Uma possível melhoria seria deixar as opções rodando até que o cliente escolha sair do programa, para que ele possa
      realizar múltiplas operações sem precisar reiniciar o programa.
      
      '''
